34884376d2778ae16be00c3c9944a7976352b056
Lukasz Rohde <l.rohde@kainos.com>
2017-05-30 14:52:06 +0200

    update download url

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@
 #
 # ```puppet
 # class {'caddy':
-#   caddy_features = "git,mailout,ipfilter",
+#   caddy_features = "http.filter,http.git,http.ipfilter",
 # }
 # ```
 #
@@ -28,13 +28,12 @@
 #
 class caddy (
 
-  $install_path  = $caddy::params::install_path,
-  $caddy_user    = $caddy::params::caddy_user,
-  $caddy_group   = $caddy::params::caddy_group,
-  $caddy_log_dir = $caddy::params::caddy_log_dir,
-  $caddy_tmp_dir = $caddy::params::caddy_tmp_dir,
-
-  $caddy_features = 'git,mailout,ipfilter',
+  $install_path      = $caddy::params::install_path,
+  $caddy_user        = $caddy::params::caddy_user,
+  $caddy_group       = $caddy::params::caddy_group,
+  $caddy_log_dir     = $caddy::params::caddy_log_dir,
+  $caddy_tmp_dir     = $caddy::params::caddy_tmp_dir,
+  $caddy_features    = 'http.filter,http.git,http.ipfilter'
 
   )inherits caddy::params{
 
--- manifests/package.pp
+++ manifests/package.pp
@@ -9,11 +9,12 @@ class caddy::package inherits caddy {
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
   }
 
-  $caddy_url    ="https://caddyserver.com/download/build?os=linux\\&arch=${$caddy::params::arch}\\&features=${caddy::caddy_features}"
-  $caddy_dl_dir ="-o ${caddy::params::caddy_tmp_dir}/caddy_linux_${$caddy::params::arch}_custom.tar.gz"
+  $caddy_url    = 'https://caddyserver.com/download/linux'
+  $caddy_dl_url = "${caddy_url}/${$caddy::params::arch}?plugins=${caddy::caddy_features}"
+  $caddy_dl_dir = "${caddy::params::caddy_tmp_dir}/caddy_linux_${$caddy::params::arch}_custom.tar.gz"
 
   exec { 'install caddy':
-    command => "curl ${caddy_dl_dir} ${caddy_url}",
+    command => "curl -o ${caddy_dl_dir} ${caddy_dl_url}",
     creates => "${caddy::install_path}/caddy",
   }
 

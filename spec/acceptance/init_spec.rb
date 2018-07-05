fd00642236539abee520320d4091aed961f222a2
Lukasz Rohde <l.rohde@kainos.com>
2018-07-05 14:32:17 +0200

    spec update

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper_acceptance'
 
 describe 'class caddy:' do
   context 'with defaults:' do
+    pp = 'include ::caddy'
     it 'runs successfully' do
-      pp = 'include ::caddy'
-
       apply_manifest(pp, catch_failures: true) do |r|
         expect(r.stderr).not_to match(%r{error}i)
       end
     end
+
     it 'runs without changes' do
       apply_manifest(pp, catch_failures: true) do |r|
         expect(r.exit_code).to be_zero
@@ -16,14 +16,14 @@ describe 'class caddy:' do
     end
   end
   context 'with vhosts' do
+    pp = "include ::caddy
+          caddy::vhost {'example1':
+            source => 'puppet:///modules/caddy/etc/caddy/config/example1.conf',
+          }
+          caddy::vhost {'example2':
+            source => 'puppet:///modules/caddy/etc/caddy/config/example2.conf',
+          }"
     it 'runs successfully' do
-      pp = "include ::caddy
-            caddy::vhost {'example1':
-              source => 'puppet:///modules/caddy/etc/caddy/config/example1.conf',
-            }
-            caddy::vhost {'example2':
-              source => 'puppet:///modules/caddy/etc/caddy/config/example2.conf',
-            }"
       apply_manifest(pp, catch_failures: true) do |r|
         expect(r.stderr).not_to match(%r{error}i)
       end

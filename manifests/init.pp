2ae4002c51b98154af00ef34ea974a6954a52ed3
Lukasz Rohde <l.rohde@kainos.com>
2018-07-05 15:58:08 +0200

    add data types

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,15 +28,15 @@
 #
 class caddy (
 
-  $install_path      = $caddy::params::install_path,
-  $caddy_user        = $caddy::params::caddy_user,
-  $caddy_group       = $caddy::params::caddy_group,
-  $caddy_log_dir     = $caddy::params::caddy_log_dir,
-  $caddy_tmp_dir     = $caddy::params::caddy_tmp_dir,
-  $caddy_home        = $caddy::params::caddy_home,
-  $caddy_license     = 'personal',
-  $caddy_telemetry   = 'off',
-  $caddy_features    = 'http.filter,http.git,http.ipfilter'
+  String                         $install_path    = $caddy::params::install_path,
+  String                         $caddy_user      = $caddy::params::caddy_user,
+  String                         $caddy_group     = $caddy::params::caddy_group,
+  String                         $caddy_log_dir   = $caddy::params::caddy_log_dir,
+  String                         $caddy_tmp_dir   = $caddy::params::caddy_tmp_dir,
+  String                         $caddy_home      = $caddy::params::caddy_home,
+  Enum['personal', 'commercial'] $caddy_license   = 'personal',
+  Enum['on','off']               $caddy_telemetry = 'off',
+  String                         $caddy_features  = 'http.filter,http.git,http.ipfilter'
 
   )inherits caddy::params{
 

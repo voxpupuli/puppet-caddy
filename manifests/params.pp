62ad7ccaf3d1477a1a1a0d27256b9bfe871b9d9b
Kristof Vandam <kristof.vandam@gmail.com>
2018-12-20 11:36:25 +0100

    alternative HTTP/S ports, fix download commercial license, alternative ssl directory

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,12 +15,22 @@ class caddy::params {
 
   case $::osfamily {
     'RedHat':  {
-      $install_path  = '/usr/local/bin'
-      $caddy_user    = 'caddy'
-      $caddy_home    = '/etc/ssl/caddy'
-      $caddy_group   = 'caddy'
-      $caddy_log_dir = '/var/log/caddy'
-      $caddy_tmp_dir = '/tmp'
+      $install_path          = '/usr/local/bin'
+      $caddy_home            = '/etc/ssl/caddy'
+      $caddy_user            = 'caddy'
+      $caddy_group           = 'caddy'
+      $caddy_license         = 'personal'
+      $caddy_account_id      = undef
+      $caddy_api_key         = undef
+      $caddy_telemetry       = 'off'
+      $caddy_features        = 'http.filter,http.git,http.ipfilter'
+      $caddy_log_dir         = '/var/log/caddy'
+      $caddy_tmp_dir         = '/tmp'
+      $caddy_ssl_dir         = "${caddy_home}/.caddy"
+      $caddy_http_port       = 80
+      $caddy_https_port      = 443
+      $caddy_private_devices = true
+      $caddy_limit_processes = 64
     }
 
     default:  {

f36adf9e0d9e7c0d3c9a0330c9ac7fc9f95d6c81
Quien Sabe <qs5779@mail.com>
2020-04-24 12:19:24 -0400

    Support for Redhat 8. Optimized for additional OS support.
    Added beaker tests for centos8. Fixed rubocop updates.
    
    Changed fail on arch to warning.
    
    Params manifest no longer used.
    
    Removed version key from hieradata.
    
    Support parameterized untested architectures, with warning.

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,16 +42,18 @@ class caddy (
   Stdlib::Port                   $caddy_https_port,
   Boolean                        $caddy_private_devices,
   Integer                        $caddy_limit_processes,
+  String                         $caddy_architecture = $facts['os']['architecture'],
   Optional[String[1]]            $caddy_account_id = undef,
   Optional[String[1]]            $caddy_api_key = undef,
 
   )
 {
-  case $facts['os']['architecture'] {
+  case $caddy_architecture {
     'x86_64', 'amd64': { $arch = 'amd64'}
     'x86'            : { $arch = '386' }
     default:  {
-      warning("arch ${facts['os']['architecture']} may not be supported.")
+      $arch = $caddy_architecture
+      warning("arch ${arch} may not be supported.")
     }
   }
 

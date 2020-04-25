3edcb55eca2f134088f629b7e265a1dd252955ed
Dennis Hoppe <github@debian-solutions.de>
2020-04-25 17:23:51 +0200

    Refactor Puppet manifests

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,18 +3,31 @@
 #
 # Manage caddy service
 #
-class caddy::service inherits caddy {
+class caddy::service (
+  $install_path          = $caddy::install_path,
+  $caddy_user            = $caddy::caddy_user,
+  $caddy_group           = $caddy::caddy_group,
+  $caddy_log_dir         = $caddy::caddy_log_dir,
+  $caddy_tmp_dir         = $caddy::caddy_tmp_dir,
+  $caddy_ssl_dir         = $caddy::caddy_ssl_dir,
+  $caddy_home            = $caddy::caddy_home,
+  $caddy_http_port       = $caddy::caddy_http_port,
+  $caddy_https_port      = $caddy::caddy_https_port,
+  $caddy_private_devices = $caddy::caddy_private_devices,
+  $caddy_limit_processes = $caddy::caddy_limit_processes,
+) {
+
+  assert_private()
 
   case $facts['os']['release']['major'] {
     '7': {
-      file {'/etc/systemd/system/caddy.service':
+      file { '/etc/systemd/system/caddy.service':
         ensure  => file,
         mode    => '0744',
         owner   => 'root',
         group   => 'root',
         content => template('caddy/etc/systemd/system/caddy.service.erb'),
         notify  => Exec['systemctl-daemon-reload'],
-        require => Class['caddy::package'],
       }
 
       exec {'systemctl-daemon-reload':
@@ -24,13 +37,12 @@ class caddy::service inherits caddy {
       }
     }
     '6': {
-      file {'/etc/init.d/caddy':
+      file { '/etc/init.d/caddy':
         ensure  => file,
         mode    => '0744',
         owner   => 'root',
         group   => 'root',
         content => template('caddy/etc/init.d/caddy.erb'),
-        require => Class['caddy::package'],
       }
     }
     default:  {
@@ -38,7 +50,7 @@ class caddy::service inherits caddy {
     }
   }
 
-  service{'caddy':
+  service{ 'caddy':
     ensure => running,
     enable => true,
   }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,6 +11,9 @@ describe 'caddy' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('caddy') }
         it { is_expected.to contain_class('caddy::params') }
+        it { is_expected.to contain_class('caddy::package').that_comes_before('Class[caddy::config]') }
+        it { is_expected.to contain_class('caddy::config').that_notifies('Class[caddy::service]') }
+        it { is_expected.to contain_class('caddy::service') }
         it do
           is_expected.to contain_group('caddy').with(
             'ensure' => 'present',
@@ -25,6 +28,118 @@ describe 'caddy' do
             'system'     => 'true',
             'home'       => '/etc/ssl/caddy',
             'managehome' => 'true'
+          )
+        end
+
+        it do
+          is_expected.to contain_exec('install caddy').with(
+            'command' => 'curl -o /tmp/caddy_linux_amd64_custom.tar.gz \'https://caddyserver.com/download/linux/amd64?plugins=http.filter,http.git,http.ipfilter&license=personal&telemetry=off\'',
+            'creates' => '/usr/local/bin/caddy'
+          )
+        end
+        it do
+          is_expected.to contain_exec('extract caddy').with(
+            'command' => 'tar -zxf /tmp/caddy_linux_amd64_custom.tar.gz -C /usr/local/bin \'caddy\'',
+            'creates' => '/usr/local/bin/caddy',
+            'require' => 'Exec[install caddy]'
+          )
+        end
+        it do
+          is_expected.to contain_file('/usr/local/bin/caddy').with(
+            'mode'    => '0755',
+            'owner'   => 'root',
+            'group'   => 'root',
+            'notify'  => 'Exec[set cap caddy]',
+            'require' => 'Exec[extract caddy]'
+          )
+        end
+        it do
+          is_expected.to contain_exec('set cap caddy').with(
+            'command'     => 'setcap cap_net_bind_service=+ep /usr/local/bin/caddy',
+            'require'     => 'File[/usr/local/bin/caddy]',
+            'refreshonly' => 'true'
+          )
+        end
+
+        it do
+          is_expected.to contain_file('/etc/ssl/caddy/.caddy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'caddy',
+            'group'   => 'caddy',
+            'mode'    => '0755'
+          )
+        end
+        it do
+          is_expected.to contain_file('/var/log/caddy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'caddy',
+            'group'   => 'caddy',
+            'mode'    => '0755'
+          )
+        end
+        it do
+          is_expected.to contain_file('/etc/caddy').with(
+            'ensure'  => 'directory',
+            'owner'   => 'root',
+            'group'   => 'root',
+            'mode'    => '0755'
+          )
+        end
+        it do
+          is_expected.to contain_file('/etc/caddy/Caddyfile').with(
+            'ensure'  => 'file',
+            'owner'   => 'caddy',
+            'group'   => 'caddy',
+            'mode'    => '0444',
+            'source'  => 'puppet:///modules/caddy/etc/caddy/Caddyfile',
+            'require' => 'File[/etc/caddy]'
+          )
+        end
+        it do
+          is_expected.to contain_file('/etc/caddy/config').with(
+            'ensure'  => 'directory',
+            'purge'   => 'true',
+            'recurse' => 'true',
+            'owner'   => 'caddy',
+            'group'   => 'caddy',
+            'mode'    => '0755'
+          )
+        end
+
+        case facts[:os]['release']['major']
+        when '7'
+          it do
+            is_expected.to contain_file('/etc/systemd/system/caddy.service').with(
+              'ensure'  => 'file',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0744',
+              'content' => %r{User=caddy},
+              'notify'  => 'Exec[systemctl-daemon-reload]'
+            )
+          end
+          it do
+            is_expected.to contain_exec('systemctl-daemon-reload').with(
+              'refreshonly' => 'true',
+              'path'        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+              'command'     => 'systemctl daemon-reload'
+            )
+          end
+        when '6'
+          it do
+            is_expected.to contain_file('/etc/init.d/caddy').with(
+              'ensure'  => 'file',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0744',
+              'content' => %r{DAEMONUSER=caddy}
+            )
+          end
+        end
+        it do
+          is_expected.to contain_service('caddy').with(
+            'ensure' => 'running',
+            'enable' => 'true'
           )
         end
       end

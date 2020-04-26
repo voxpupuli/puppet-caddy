51e82b84ce7fa3720cb816cd65b09730a679c0cc
Dennis Hoppe <github@debian-solutions.de>
2020-04-26 14:58:23 +0200

    Add support for puppet/archive

--- manifests/package.pp
+++ manifests/package.pp
@@ -25,33 +25,24 @@ class caddy::package (
   $caddy_url        = 'https://caddyserver.com/download/linux'
   $caddy_dl_url     = "${caddy_url}/${arch}?plugins=${caddy_features}&license=${caddy_license}&telemetry=${caddy_telemetry}"
   $caddy_dl_dir     = "${caddy_tmp_dir}/caddy_linux_${$arch}_custom.tar.gz"
-  $caddy_dl_command = $caddy_license ? {
-    'personal'   => "curl -o ${caddy_dl_dir} '${caddy_dl_url}'",
-    'commercial' => "curl -o ${caddy_dl_dir} '${caddy_dl_url}' --user ${caddy_account_id}:${caddy_api_key}"
-  }
 
-  exec { 'install caddy':
-    command => $caddy_dl_command,
-    creates => "${install_path}/caddy",
-  }
-
-  exec { 'extract caddy':
-    command => "tar -zxf ${caddy_tmp_dir}/caddy_linux_${$arch}_custom.tar.gz -C ${install_path} 'caddy'",
-    creates => "${install_path}/caddy",
-    require => Exec['install caddy'],
-  }
-
-  file { "${install_path}/caddy":
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    require => Exec['extract caddy'],
-    notify  => Exec['set cap caddy'],
+  archive { $caddy_dl_dir:
+    ensure       => present,
+    extract      => true,
+    extract_path => $install_path,
+    source       => $caddy_dl_url,
+    username     => $caddy_account_id,
+    password     => $caddy_api_key,
+    user         => 'root',
+    group        => 'root',
+    creates      => "${install_path}/caddy",
+    cleanup      => true,
+    notify       => Exec['set cap caddy'],
   }
 
   exec { 'set cap caddy':
     command     => "setcap cap_net_bind_service=+ep ${install_path}/caddy",
-    require     => File["${install_path}/caddy"],
+    require     => Archive[$caddy_dl_dir],
     refreshonly => true,
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -32,31 +32,22 @@ describe 'caddy' do
         end
 
         it do
-          is_expected.to contain_exec('install caddy').with(
-            'command' => 'curl -o /tmp/caddy_linux_amd64_custom.tar.gz \'https://caddyserver.com/download/linux/amd64?plugins=http.filter,http.git,http.ipfilter&license=personal&telemetry=off\'',
-            'creates' => '/usr/local/bin/caddy'
-          )
-        end
-        it do
-          is_expected.to contain_exec('extract caddy').with(
-            'command' => 'tar -zxf /tmp/caddy_linux_amd64_custom.tar.gz -C /usr/local/bin \'caddy\'',
-            'creates' => '/usr/local/bin/caddy',
-            'require' => 'Exec[install caddy]'
-          )
-        end
-        it do
-          is_expected.to contain_file('/usr/local/bin/caddy').with(
-            'mode'    => '0755',
-            'owner'   => 'root',
-            'group'   => 'root',
-            'notify'  => 'Exec[set cap caddy]',
-            'require' => 'Exec[extract caddy]'
+          is_expected.to contain_archive('/tmp/caddy_linux_amd64_custom.tar.gz').with(
+            'ensure'       => 'present',
+            'extract'      => 'true',
+            'extract_path' => '/usr/local/bin',
+            'source'       => 'https://caddyserver.com/download/linux/amd64?plugins=http.filter,http.git,http.ipfilter&license=personal&telemetry=off',
+            'user'         => 'root',
+            'group'        => 'root',
+            'creates'      => '/usr/local/bin/caddy',
+            'cleanup'      => 'true',
+            'notify'       => 'Exec[set cap caddy]'
           )
         end
         it do
           is_expected.to contain_exec('set cap caddy').with(
             'command'     => 'setcap cap_net_bind_service=+ep /usr/local/bin/caddy',
-            'require'     => 'File[/usr/local/bin/caddy]',
+            'require'     => 'Archive[/tmp/caddy_linux_amd64_custom.tar.gz]',
             'refreshonly' => 'true'
           )
         end

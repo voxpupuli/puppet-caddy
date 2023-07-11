70dfd36f9e41a708ce6cf4d4dc09907ad4262537
Jamie Bliss <jamie@ivyleav.es>
2023-07-11 13:40:30 -0400

    Add tests

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -18,6 +18,17 @@ describe 'class caddy:' do
     end
   end
 
+  context 'from github:' do
+    pp = "class { 'caddy':
+            install_method => 'github',
+          }"
+    it 'installs successfully' do
+      apply_manifest(pp, catch_failures: true) do |r|
+        expect(r.stderr).not_to match(%r{error}i)
+      end
+    end
+  end
+
   context 'with vhosts' do
     pp = "include caddy
           caddy::vhost {'example1':

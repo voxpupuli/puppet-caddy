7def4c92f34107dc70bdda23468ddf295c866d9b
Romain Tartière <romain@blogreen.org>
2023-07-12 11:12:06 -1000

    Test if the installed version is the requested one

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -2,6 +2,12 @@
 
 require 'spec_helper_acceptance'
 
+# The default configuration download the latest available release. In order to
+# avoid to maintain the test suite to match each release, query GitHub API to
+# find the last release.
+latest_release = JSON.parse(URI.open('https://api.github.com/repos/caddyserver/caddy/releases/latest').read)['tag_name']
+
+# rubocop:disable RSpec/RepeatedExampleGroupDescription
 describe 'class caddy:' do
   context 'with default settings' do
     it_behaves_like 'an idempotent resource' do
@@ -12,6 +18,10 @@ describe 'class caddy:' do
         PUPPET
       end
     end
+
+    describe command('/opt/caddy/caddy version') do
+      its(:stdout) { is_expected.to start_with latest_release }
+    end
   end
 
   context 'when installing from GitHub' do
@@ -20,9 +30,29 @@ describe 'class caddy:' do
         <<~PUPPET
           class { 'caddy':
             install_method => 'github',
+            version        => '2.6.0',
           }
         PUPPET
       end
+    end
+
+    describe command('/opt/caddy/caddy version') do
+      its(:stdout) { is_expected.to start_with 'v2.6.0' }
+    end
+
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<~PUPPET
+          class { 'caddy':
+            install_method => 'github',
+            version        => '#{latest_release.sub(%r{\Av}, '')}',
+          }
+        PUPPET
+      end
+    end
+
+    describe command('/opt/caddy/caddy version') do
+      its(:stdout) { is_expected.to start_with latest_release }
     end
   end
 
@@ -45,3 +75,4 @@ describe 'class caddy:' do
     end
   end
 end
+# rubocop:enable RSpec/RepeatedExampleGroupDescription
